03201d1097fb9fab53f1c96c8cc58d526b4e6191
杨浩楠 <yanghaonan@aipark.com>
2025-04-18 11:35:40 +0800

    ✨ feat: 更新 pz

--- pinzan.js
+++ pinzan.js
@@ -5,7 +5,7 @@
   date:2024-11-12
   cron: 0 10 ? * MON
 */
-const pzhttp = "18888888888#111111" //格式：账号#密码
+const pzhttp = os.environ.get("pinzan","")
 const axios = require("axios")
 let d = {
   table: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "/"],
